51c2680fe4f240cda128cf5787a5dbf945ec4b87
Daniel Merritt <daniel.mafou@gmail.com>
2023-05-13 13:18:34 -0400

    finish getRecipeData

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,11 +1,7 @@
 async function getRecipeData(req, res){
-  const apiResponse = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
+  const apiResponse = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${req.query.q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
   const recipeData = await apiResponse.json()
   res.json(recipeData)
-  
-  
-  
-  
 }
 
 export {
