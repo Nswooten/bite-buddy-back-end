ddb02217964cf80f7458da8731207433919670b9
Nick Wooten <nswooten@gmail.com>
2023-05-13 11:48:35 -0500

    setup api fetch getRecipeData

---
+++ controllers/recipes.js
@@ -0,0 +1,16 @@
+async function getRecipeData(req, res){
+  const apiResponse = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
+  const recipeData = await apiResponse.json()
+  res.json(recipeData)
+  
+  
+  
+  
+}
+
+export {
+  getRecipeData,
+
+}
+
+
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import * as recipesCtrl from '../controllers/recipes.js'
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
+
 const router = Router()
 
 // ========== Public Routes ===========
-
+router.get('/', recipesCtrl.getRecipeData)
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
